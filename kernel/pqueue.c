c46c0ab5ab161339c2fb5f101213dcda39fa281d
23scurtu <23scurtuvictor@gmail.com>
2020-01-19 02:36:43 -0500

    Change priority queue to use linked lists

--- kernel/pqueue.c
+++ kernel/pqueue.c
@@ -2,8 +2,11 @@
 
 void init_pqueue(struct pqueue* pq)
 {
-    // Currently the queues must be initialized within kmain 
-    for(int i = 0; i != MIN_PRIORITY+1; i++) pq->queue_starts[i] = 0;
+    pq->next_available_node = MAX_TASKS_ALLOWED*(MIN_PRIORITY+1) -1;
+    for(int i = 0; i != MAX_TASKS_ALLOWED*(MIN_PRIORITY+1); i++) pq->available_nodes[i] = &(pq->nodes[i]);
+    for(int i = 0; i != MAX_TASKS_ALLOWED; i++) pq->id_to_node[i] = 0;
+    
+    for(int i = 0; i != MIN_PRIORITY+1; i++) pq->queue_fronts[i] = 0;
     for(int i = 0; i != MIN_PRIORITY+1; i++) pq->queue_ends[i] = 0;
     for(int i = 0; i != MIN_PRIORITY+1; i++) pq->queue_sizes[i] = 0;
 }
@@ -11,30 +14,123 @@ void init_pqueue(struct pqueue* pq)
 void add_task(pqueue* pq, int id, int priority)
 {
     if(id >= MAX_TASKS_ALLOWED) return;
+    if(pq->next_available_node == -1) return;
     
-    pq->queue_ends[priority] = (pq->queue_ends[priority] + 1)%(MIN_PRIORITY+1);
-    pq->queues[priority][pq->queue_ends[priority]] = id;
+    // "Allocate" new pq_node
+    pq_node* new_node = pq->available_nodes[pq->next_available_node];
+    pq->next_available_node--;
+    pq->id_to_node[id] = new_node;
+
+    new_node->id = id;
+    new_node->priority = priority;
+    new_node->next = 0;
+    new_node->prev = pq->queue_ends[priority];
+    
+    if(pq->queue_fronts[priority] == 0) pq->queue_fronts[priority] = new_node;
+    
+    pq->queue_ends[priority] = new_node;
     pq->queue_sizes[priority]++;
 }
 
-int remove_task(pqueue* pq, int priority)
+void remove_task(pqueue* pq, int id, int priority)
 {
-    if(pq->queue_sizes[priority] == 0) return 0; // TODO Define 0 as invalid task
+    pq_node* node = pq->id_to_node[id];
 
-    int r = pq->queues[priority][pq->queue_starts[priority]];
-    pq->queue_starts[priority] = (pq->queue_starts[priority] + 1)%(MIN_PRIORITY+1);
+    if(node == 0) return;
+
+    if(node->next && node->prev)
+    {
+        node->next->prev = node->prev;
+        node->prev->next = node->next;
+    }
+    else if(node->next) // node->prev == 0
+    {
+        pq->queue_fronts[priority] = node->next;
+        node->next->prev = 0;
+    }
+    else if(node->prev) // node->next == 0
+    {
+        pq->queue_ends[priority] = node->prev;
+        node->prev->next = 0;
+    }
+    else
+    {
+        pq->queue_ends[priority] = 0;
+        pq->queue_fronts[priority] = 0;
+    }
+
+    // "Deallocate" the node
+    pq->next_available_node++;
+    pq->available_nodes[pq->next_available_node] = node;
+    pq->id_to_node[node->id] = 0;
     pq->queue_sizes[priority]--;
+}
+
+int pop_task(pqueue* pq, int priority)
+{
+    if(pq->queue_fronts[priority] == 0) return 0;
     
+    pq_node* node = pq->queue_fronts[priority];
+    int r = node->id;
+
+    if(node->next) node->next->prev = 0;
+    pq->queue_fronts[priority] = node->next;
+
+    pq->next_available_node++;
+    pq->available_nodes[pq->next_available_node] = node;
+    pq->id_to_node[node->id] = 0;
+    pq->queue_sizes[priority]--;
+
     return r;
 }
 
 int front_task(pqueue* pq, int priority)
 {   
-    if(pq->queue_sizes[priority] == 0) return 0;
-    return pq->queues[priority][pq->queue_starts[priority]];
+    if(pq->queue_fronts[priority] == 0) return 0;
+    return pq->queue_fronts[priority]->id;
 }
 
 uint pq_size(pqueue* pq, int priority)
 {
     return pq->queue_sizes[priority];
 }
+
+// OLD PQUEUE IMPLEMENTATION
+// void init_pqueue(struct pqueue* pq)
+// {
+//     // Currently the queues must be initialized within kmain 
+//     for(int i = 0; i != MIN_PRIORITY+1; i++) pq->queue_starts[i] = 0;
+//     for(int i = 0; i != MIN_PRIORITY+1; i++) pq->queue_ends[i] = 0;
+//     for(int i = 0; i != MIN_PRIORITY+1; i++) pq->queue_sizes[i] = 0;
+// }
+
+// void add_task(pqueue* pq, int id, int priority)
+// {
+//     if(id >= MAX_TASKS_ALLOWED) return;
+    
+//     pq->queue_ends[priority] = (pq->queue_ends[priority] + 1)%(MIN_PRIORITY+1);
+//     pq->queues[priority][pq->queue_ends[priority]] = id;
+//     pq->queue_sizes[priority]++;
+// }
+
+// int remove_task(pqueue* pq, int priority)
+// {
+//     if(pq->queue_sizes[priority] == 0) return 0; // TODO Define 0 as invalid task
+
+//     int r = pq->queues[priority][pq->queue_starts[priority]];
+//     pq->queue_starts[priority] = (pq->queue_starts[priority] + 1)%(MIN_PRIORITY+1);
+//     pq->queue_sizes[priority]--;
+    
+//     return r;
+// }
+
+// int front_task(pqueue* pq, int priority)
+// {   
+//     if(pq->queue_sizes[priority] == 0) return 0;
+//     return pq->queues[priority][pq->queue_starts[priority]];
+// }
+
+// uint pq_size(pqueue* pq, int priority)
+// {
+//     return pq->queue_sizes[priority];
+// }
--- kernel/pqueue.h
+++ kernel/pqueue.h
@@ -1,25 +1,47 @@
 #ifndef PQUEUE_H
 #define PQUEUE_H
 
-
 #include "constants.h"
+
+typedef struct pqueue pqueue;
+typedef struct pq_node pq_node;
+
+struct pq_node
+{
+    uint id;
+    uint priority;
+    pq_node* prev;
+    pq_node* next;
+};
 
 struct pqueue
 {
-    int queues[MAX_TASKS_ALLOWED][MIN_PRIORITY+1];
-    uint queue_starts[MIN_PRIORITY+1];
-    uint queue_ends[MIN_PRIORITY+1];
+    pq_node nodes[MAX_TASKS_ALLOWED*(MIN_PRIORITY+1)];
+    pq_node *id_to_node[MAX_TASKS_ALLOWED];
+    pq_node *available_nodes[MAX_TASKS_ALLOWED*(MIN_PRIORITY+1)];
+    int next_available_node;
+
+    pq_node *queue_fronts[MIN_PRIORITY+1];
+    pq_node *queue_ends[MIN_PRIORITY+1];
     uint queue_sizes[MIN_PRIORITY+1];
 };
 
-typedef struct pqueue pqueue;
-
 void init_pqueue(struct pqueue* pq);
 
-// add_task and remove_task are FIFO
+// add_task and pop_task are FIFO
 void add_task(pqueue* pq, int id, int priority);
-int remove_task(pqueue* pq, int priority);
+void remove_task(pqueue* pq, int id, int priority);
+int pop_task(pqueue* pq, int priority);
 int front_task(pqueue* pq, int priority);
 uint pq_size(pqueue* pq, int priority);
+
+// OLD PQUEUE IMPLEMENTATION
+// struct pqueue
+// {
+//     int queues[MAX_TASKS_ALLOWED][MIN_PRIORITY+1];
+//     uint queue_starts[MIN_PRIORITY+1];
+//     uint queue_ends[MIN_PRIORITY+1];
+//     uint queue_sizes[MIN_PRIORITY+1];
+// };
 
 #endif
