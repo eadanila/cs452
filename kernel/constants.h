5de6d92a3d0d01c29d3147b78ccc6257516f0254
23scurtu <23scurtuvictor@gmail.com>
2020-01-24 11:50:51 -0500

    Change maximum tasks allowed and task memory size

--- kernel/constants.h
+++ kernel/constants.h
@@ -9,8 +9,8 @@ typedef unsigned int uint;
 
 #define MEMORY_END 0x2000000
 #define MEMORY_START 0x0100000
-#define TASK_MEMORY_SIZE 0x04000 // 16 KiB
-#define MAX_TASKS_ALLOWED 1024
+#define TASK_MEMORY_SIZE 0x20000 // 128 KiB
+#define MAX_TASKS_ALLOWED 128
 #define MIN_PRIORITY 7
 #define PRIORITY_COUNT (MIN_PRIORITY + 1)
 
