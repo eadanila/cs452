65df4871f3eaaa071700c24c4ee08aa4a7a40e49
Alexandru Danila <eadanila@uwaterloo.ca>
2020-02-07 20:04:55 -0500

    Add: handle interrupts and timing
    Measure time running idle task vs time running.
    Interrupt handling done properly.

--- kernel/main.c
+++ kernel/main.c
@@ -4,16 +4,12 @@
 #include "arm_lib.h"
 #include "syscall.h"
 #include "pqueue.h"
-
 #include "logging.h"
-
 #include "user.h"
-
 #include "timer.h"
-
 #include "frame.h"
-
 #include "interrupt.h"
+#include "idle.h"
 
 int main(int argc, char *argv[]) {
     kinit();
@@ -22,43 +18,50 @@ int main(int argc, char *argv[]) {
 
 
     uint cpsr_mode = 0x13;
-
+    
+    int idle_task_id = kcreate(7, (uint)idle_task);
+    DEBUG("Idle task ID: %d", idle_task_id);
     int id = kcreate(3, (uint)umain);
 
+    idle_time = 0;
+
+    // sanity check
     int sid = pop_task();
     if (id != sid)
         FATAL("Task not successfully scheduled. Got %d, expected %d", sid, id);
+
     set_running_task(id);
+
+    stop_debug_timer();
+    start_debug_timer();
 
     for (;;) {
         LOG("Got ID %d from PQ %d", id, get_task_by_id(id).priority);
 
-        if (cpsr_mode == 0x12) {
-            handle_interrupt();
+        start_time = read_debug_timer();
+        set_task_stack_pointer(id, enter_user(get_task_stack_pointer(id)));
+        end_time = read_debug_timer();
+
+        if (id == idle_task_id) {
+            idle_time += (end_time - start_time);
         }
 
-        set_task_stack_pointer(id, enter_user(get_task_stack_pointer(id)));
         cpsr_mode = get_cpsr() & 0x1F;
+        DEBUG("Processor mode: %x", cpsr_mode);
 
         if (cpsr_mode == 0x12) {
             DEBUG("IRQ CAUGHT");
-            Frame *fp = (Frame *)get_task_stack_pointer(id);
-            fp->r0 = 1;
-            handle_swi(id);
+            handle_interrupt(id);
         }
         else if (cpsr_mode == 0x13) {
             DEBUG("SWI CAUGHT");
-            *((volatile uint *)0x8092008) = 0;
             handle_swi(id);
+            if (get_active_tasks_count() == 1)
+                break;
         }
-        else
-            panic();
 
         id = pop_task();
         set_running_task(id);
-
-        if (id < 0)
-            break;
     }
 
     kcleanup();
